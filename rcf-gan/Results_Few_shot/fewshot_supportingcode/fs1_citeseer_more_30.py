ba909bc489e53b3de0f92161d7f622d75398eeb4
Adrian Kwan <86300407+AdrianKPC@users.noreply.github.com>
2023-07-07 20:30:53 +0800

    Add files via upload

---
+++ rcf-gan/Results_Few_shot/fewshot_supportingcode/fs1_citeseer_more_30.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+import numpy as np
+import matplotlib.pyplot as plt
+from fs1_citeseer_more_cfgan_30 import Model
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+import numpy as np
+
+# training epoch
+#epoch = 50
+epoch = 200
+
+load = False
+
+# hyperparameters for target image data
+target_source = 'citeseer'  # cora citeseer pubmed nell.0.1
+target_batch_size = 64
+feature_dim = 128
+
+#input, ie noise dimension
+target_size = 0  # 0 means using all data samples
+
+ae_loss_reg = np.linspace(0.6, 1, 300)
+
+# hyperparameters for generating nets (Note: activations are only valid for dcgan)
+generator_training_param = {'input_noise_dim': feature_dim,
+                            'input_noise_var': 0.3, 'net_type': 'gnn', 'lr': 2e-4, 'inner_ite_per_batch': 1,
+                            'activations': [('relu', None), ('sigmoid', None)], 'net_dim': [128],
+                            'weight_decay': 0, 'lr_step_size_decay': 0, 'lr_decay_gamma': 0.5}
+
+# Hyperparameters for adversarial nets (Note: activations_a are only valid for dcgan)
+adversarial_training_param = {'input_t_dim': feature_dim, 'input_t_batchsize': target_batch_size,
+                              'input_t_var': 1, 'net_type': 'gnn', 'lr': 2e-4, 'inner_ite_per_batch': 1,
+                              'activations_a': [('lrelu', 0.2), ('tanh', None)], 'net_dim': [128],
+                              'weight_decay': 0, 'lr_step_size_decay': 0, 'lr_decay_gamma': 0.5,
+                              'adv_t_sigma_num': target_batch_size,
+                              'activations_t': [('lrelu', 0.2), ('tanh', None)]}
+
+# hyperparameters for CFLossFunc
+loss_alpha = 1  # amplitude
+loss_beta =  1 # phase
+# threshold is useless when normalization is None
+loss_type = None
+loss_threshold = 1
+
+# training
+mark = 'link_prediction'
+if adversarial_training_param['adv_t_sigma_num'] > 0:
+    model_label = 'gl_' + target_source + '_t_net_' + generator_training_param['net_type'] + '_' + mark
+else:
+    model_label = 'gl_' + target_source + '_t_normal_' + generator_training_param['net_type'] + '_' + mark
+
+fs_model = Model(model_label,
+              target_source, target_size, target_batch_size,
+              adversarial_training_param, generator_training_param,
+              loss_type, loss_alpha, loss_beta, loss_threshold, ae_loss_reg,
+              epoch)
+
+fs_model.train(load)
+
---
+++ rcf-gan/Results_Few_shot/fs1_citeseer_more_utility_10.py
@@ -0,0 +1,421 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import scipy.sparse as sp
+import random
+
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Subset
+from torchvision import transforms, utils
+
+import pickle as pkl
+import networkx as nx
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+
+def prepare_dir(model_label):
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists('model'):
+        os.mkdir('model')
+    if not os.path.exists('result'):
+        os.mkdir('result')
+
+    # label dir
+    model_label_dir = os.path.join('model', model_label)
+    if not os.path.exists(model_label_dir):
+        os.mkdir(model_label_dir)
+    model_path = os.path.join(model_label_dir, 'final_model')
+
+    # trace dir
+    model_trace_dir = os.path.join(model_label_dir, 'trace')
+    if not os.path.exists(model_trace_dir):
+        os.mkdir(model_trace_dir)
+    model_trace_path = os.path.join(model_trace_dir, 'trace_Epoch_{0}_Loss_{1:.4}')
+
+    # result dir
+    result_dir = os.path.join('result', model_label)
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+    model_result_trace_path = os.path.join(result_dir, 'trace_Epoch_{0}.png')
+    model_result_final_dir = result_dir
+    middle_result_trace_path = os.path.join(result_dir, 'Mid_trace_Epoch_{0}.png')
+    return model_path, model_trace_path, model_result_trace_path, \
+           model_result_final_dir, middle_result_trace_path, model_label_dir
+
+
+def get_input_white_noise(white_noise_dim, white_noise_var, white_noise_num):
+    white_noise = torch.randn(white_noise_num, white_noise_dim) * (white_noise_var ** 0.5)
+    return white_noise
+
+
+def get_input_mixture_white_noise(white_noise_dim, white_noise_var, white_noise_num, mixture_num=10):
+    white_cov_noise = torch.randn(white_noise_num, white_noise_dim) * (white_noise_var ** 0.5)
+    linear_mu = torch.div(torch.randint(1, mixture_num, (white_noise_num,)), float(mixture_num * 2))
+    linear_mu = linear_mu - torch.ones_like(linear_mu)
+    linear_mu = linear_mu.unsqueeze(-1)
+    linear_mu = linear_mu.repeat(1, white_noise_dim)
+    white_noise = linear_mu + white_cov_noise
+    return white_noise
+
+
+def get_input_uniform_noise(uniform_noise_upb, uniform_noise_lowb, uniform_noise_dim, uniform_noise_num):
+    white_noise = np.random.uniform(low=uniform_noise_lowb, high=uniform_noise_upb,
+                                    size=[uniform_noise_num, uniform_noise_dim])
+    return torch.from_numpy(white_noise)
+
+# Adopted from https://github.com/tkipf/gae/blob/master/gae/input_data.py
+def get_graph_target(source):
+#    if source not in ['cora', 'citeseer', 'pubmed']:
+#        raise SystemExit('Error: Unknown source for target: {0}'.format(source))
+    # load the data: x, tx, allx, graph
+#    if source == 'cora'or 'citeseer'or 'pubmed'or 'nell.0.1':
+    names = ['x', 'tx', 'allx', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("/Users/adriankwan_mba/Downloads/grcf-master/data/ind.{}.{}".format(source, names[i]), 'rb') as rf:
+            u = pkl._Unpickler(rf)
+            u.encoding = 'latin1'
+            cur_data = u.load() #load pickled into volatile memory
+            objects.append(cur_data) #add to objects array
+        # objects.append(
+        #     pkl.load(open("data/ind.{}.{}".format(dataset, names[i]), 'rb')))
+    x, tx, allx, graph = tuple(objects) #make tuple with objects
+    test_idx_reorder = parse_index_file(
+        "/Users/adriankwan_mba/Downloads/grcf-master/data/ind.{}.test.index".format(source)) #strip of string in front and at the back, leaving only the index
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if source == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(
+            min(test_idx_reorder), max(test_idx_reorder) + 1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx = tx_extended
+
+    features = sp.vstack((allx, tx)).tolil() #scipy sparse, v stack- stack sparse matrices vertically (row wise), allx superset of x, feature vector of all training instances- both labelled and unlabelled; tx feature vector of test instances; made to list of lists
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+    features = torch.FloatTensor(np.array(features.todense())) #returns matrix form of it;
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+
+    return adj, features
+
+#get_graph_target called in cfgan_model, need to check paper for file format, and verify the output of the function
+
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
+#def get_graph_target(source):
+#    print('Loading {} dataset...'.format(source))
+#
+#    idx_features_labels = np.genfromtxt("/Users/adriankwan_mba/Downloads/grcf-master/data/{}.content".format(source),
+#                                        dtype=np.dtype(str))
+#    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+#    labels = encode_onehot(idx_features_labels[:, -1])
+#
+#    # build graph
+#    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+#    idx_map = {j: i for i, j in enumerate(idx)}
+#    edges_unordered = np.genfromtxt("/Users/adriankwan_mba/Downloads/grcf-master/data/{}.cites".format(source),
+#                                    dtype=np.int32)
+#    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+#                     dtype=np.int32).reshape(edges_unordered.shape)
+#    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+#                        shape=(labels.shape[0], labels.shape[0]),
+#                        dtype=np.float32)
+#
+#    # build symmetric adjacency matrix
+#    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+#
+#    features = normalize(features)
+#    adj = normalize(adj + sp.eye(adj.shape[0]))
+#
+##    idx_train = range(140)
+##    idx_val = range(200, 500)
+##    idx_test = range(500, 1500)
+#
+#    features = torch.FloatTensor(np.array(features.todense()))
+##    labels = torch.LongTensor(np.where(labels)[1])
+#    adj = sparse_mx_to_torch_sparse_tensor(adj)
+#
+##    idx_train = torch.LongTensor(idx_train)
+##    idx_val = torch.LongTensor(idx_val)
+##    idx_test = torch.LongTensor(idx_test)
+#
+#    return adj, features
+##    , labels, idx_train, idx_val, idx_test
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+def parse_index_file(filename):
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+### The same as https://github.com/tkipf/gae/blob/master/gae/preprocessing.py
+def mask_test_edges(adj):
+    # Function to build test set with 10% positive links
+    # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
+    # TODO: Clean up.
+
+    # Remove diagonal elements
+    adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+    adj.eliminate_zeros()
+    # Check that diag is zero:
+    assert np.diag(adj.todense()).sum() == 0
+
+    adj_triu = sp.triu(adj)
+    adj_tuple = sparse_to_tuple(adj_triu)
+    edges = adj_tuple[0]
+    edges_all = sparse_to_tuple(adj)[0]
+
+#    # setting 1
+#    num_test = int(np.floor(edges.shape[0] / 5.))  # Double the size of the test set, 20% as test edges
+#    num_val = int(np.floor(edges.shape[0] / 20.)) #5% as validation edges
+#
+#    all_edge_idx = list(range(edges.shape[0]))
+#    np.random.shuffle(all_edge_idx)
+#    val_edge_idx = all_edge_idx[:num_val]
+#    test_edge_idx = all_edge_idx[num_val:(num_val + num_test)]
+#    test_edges = edges[test_edge_idx]
+#    val_edges = edges[val_edge_idx]
+#    train_edges = np.delete(edges, np.hstack([test_edge_idx, val_edge_idx]), axis=0)#75% as training edges
+
+    # setting 2
+    num_test = int(np.floor(edges.shape[0] / 10.)) #10% kept as test edges
+    num_val = int(np.floor(edges.shape[0] / 20.)) #5% kept as validation edges
+
+    all_edge_idx = list(range(edges.shape[0]))
+    np.random.shuffle(all_edge_idx)
+    val_edge_idx = all_edge_idx[:num_val]
+    test_edge_idx = np.random.choice(all_edge_idx[num_val:(num_val + num_test)], size=int(num_test * 0.5), replace=False) #8% test edges
+    test_edges = edges[test_edge_idx]
+    val_edges = edges[val_edge_idx]
+    train_edges = np.delete(edges, np.hstack([test_edge_idx, val_edge_idx]), axis=0) #85% as training edges
+
+    frac = 0.1  #Preserve a fraction of train_edges to preserve
+    num_train = int(frac * len(train_edges))
+    
+    random.seed()
+    preserved_indices = random.sample(range(len(train_edges)), num_train)
+    train_edges = train_edges[preserved_indices]
+    # train_edges = train_edges[:num_train]
+
+
+    def ismember(a, b, tol=5):
+        rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
+        return np.any(rows_close)
+
+    test_edges_false = []
+    while len(test_edges_false) < len(test_edges):
+        idx_i = np.random.randint(0, adj.shape[0])
+        idx_j = np.random.randint(0, adj.shape[0])
+        if idx_i == idx_j:
+            continue
+        if ismember([idx_i, idx_j], edges_all):
+            continue
+        if test_edges_false:
+            if ismember([idx_j, idx_i], np.array(test_edges_false)):
+                continue
+            if ismember([idx_i, idx_j], np.array(test_edges_false)):
+                continue
+        test_edges_false.append([idx_i, idx_j])
+
+    val_edges_false = []
+    while len(val_edges_false) < len(val_edges):
+        idx_i = np.random.randint(0, adj.shape[0])
+        idx_j = np.random.randint(0, adj.shape[0])
+        if idx_i == idx_j:
+            continue
+        if ismember([idx_i, idx_j], train_edges):
+            continue
+        if ismember([idx_j, idx_i], train_edges):
+            continue
+        if ismember([idx_i, idx_j], val_edges):
+            continue
+        if ismember([idx_j, idx_i], val_edges):
+            continue
+        if val_edges_false:
+            if ismember([idx_j, idx_i], np.array(val_edges_false)):
+                continue
+            if ismember([idx_i, idx_j], np.array(val_edges_false)):
+                continue
+        val_edges_false.append([idx_i, idx_j])
+
+    assert ~ismember(test_edges_false, edges_all)
+    assert ~ismember(val_edges_false, edges_all)
+    assert ~ismember(val_edges, train_edges)
+    assert ~ismember(test_edges, train_edges)
+    assert ~ismember(val_edges, test_edges)
+
+    data = np.ones(train_edges.shape[0])
+
+    # Re-build adj matrix
+    adj_train = sp.csr_matrix((data, (train_edges[:, 0], train_edges[:, 1])), shape=adj.shape)
+    adj_train = adj_train + adj_train.T
+
+    # NOTE: these edge lists only contain single direction of edge!
+    return adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false
+
+
+def preprocess_graph(adj):
+    adj = sp.coo_matrix(adj)
+    adj_ = adj + sp.eye(adj.shape[0])
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())
+    adj_normalized = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt).tocoo()
+    # return sparse_to_tuple(adj_normalized)
+    return sparse_mx_to_torch_sparse_tensor(adj_normalized)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+###
+
+def pdist(e, squared=False, eps=1e-12):
+    e_square = e.pow(2).sum(dim=1)
+    prod = e @ e.t()
+    res = (e_square.unsqueeze(1) + e_square.unsqueeze(0) - 2 * prod).clamp(min=eps)
+
+    if not squared:
+        res = res.sqrt()
+
+    res = res.clone()
+    res[range(len(e)), range(len(e))] = 0
+    return res
+
+
+def get_roc_score(emb, adj_orig, edges_pos, edges_neg):
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
+    # Predict on test set of edges
+    adj_rec = np.dot(emb, emb.T)
+    # adj_rec = pdist(emb)
+    # adj_rec = (adj_rec.max() - adj_rec)/adj_rec.max()
+    preds = []
+    pos = []
+    for e in edges_pos:
+        preds.append(sigmoid(adj_rec[e[0], e[1]]))
+        # preds.append((adj_rec[e[0], e[1]]))
+        pos.append(adj_orig[e[0], e[1]])
+
+    preds_neg = []
+    neg = []
+    for e in edges_neg:
+        preds_neg.append(sigmoid(adj_rec[e[0], e[1]]))
+        # preds_neg.append((adj_rec[e[0], e[1]]))
+        neg.append(adj_orig[e[0], e[1]])
+
+    preds_all = np.hstack([preds, preds_neg])
+    labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
+    roc_score = roc_auc_score(labels_all, preds_all)
+    ap_score = average_precision_score(labels_all, preds_all)
+
+    return roc_score, ap_score
+
+
+def sparse_to_tuple(sparse_mx):
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+
+def normalise_adj(adj):
+    # print(adj.sum(dim=1))
+    inv_degree = torch.pow(adj.sum(dim=1), -0.5)
+    inv_degree_matrix = torch.diag(inv_degree)
+    return torch.matmul(inv_degree_matrix, torch.matmul(adj, inv_degree_matrix))
+
+
+def save_data(x, dim, ax, channel, save_dir, padding=0):
+    x = x / 2 + 0.5
+    # examples = torch.clamp(x[:64, :].view(64, channel, dim, dim), 0, 1)
+    examples = torch.clamp(x[:64, :, :, :], 0, 1)
+    ax.axis('off')
+    ax.imshow(np.transpose(utils.make_grid(examples, padding=padding, normalize=True), (1, 2, 0)))
+    utils.save_image(examples, save_dir, padding=padding)
+
+
+def record_as_img(x, save_dir):
+    examples = x[:100, :]
+    examples = (examples + 1) / 2.
+    _, ax = plt.subplots(1)
+    ax.axis('off')
+    ax.imshow(examples)
+    utils.save_image(examples, save_dir)
+    return
+
+
+def tensorboard_img_writer(x, writer, name):
+    examples = x[:100, :]
+    examples = (examples + 1) / 2.
+    writer.add_image(name, examples)
+
+
+class avg_record(object):
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
